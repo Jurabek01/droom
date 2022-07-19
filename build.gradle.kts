910152293cb3f546dec07232b85dcc0c0aeb0307
Jamshid Qodirov <jamshidxon99@gmail.com>
2022-07-19 12:50:29 +0500

    configure monitoring was added

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ val logback_version: String by project
 
 plugins {
     application
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.6.20"
 }
 
 group = "com.example"
--- src/main/kotlin/io/mimsoft/Application.kt
+++ src/main/kotlin/io/mimsoft/Application.kt
@@ -5,12 +5,14 @@ import com.example.utils.configureSerialization
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.mimsoft.utils.plugins.configureMonitoring
 
 fun main() {
 
 
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
+        configureMonitoring()
         configureSerialization()
     }.start(wait = true)
 }
