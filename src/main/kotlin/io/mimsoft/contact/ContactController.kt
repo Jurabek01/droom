4e8f85f5e636dc6a2c215106df7885196f4e2f70
Jurabek Rustamov <jurabek.rustamov1999@gmail.com>
2022-07-19 17:57:04 +0500

    contact api

---
+++ src/main/kotlin/io/mimsoft/contact/ContactController.kt
@@ -0,0 +1,33 @@
+package io.mimsoft.contact
+
+import io.mimsoft.utils.DBManager
+import io.mimsoft.utils.sendPreparedStatementAwait
+
+object ContactController {
+
+    suspend fun get(): ContactModel?{
+        val query = "select * from contact "
+
+        return DBManager.getConnection().sendPreparedStatementAwait(query, arrayListOf()).rows.getOrNull(0)?.let {
+            ContactModel(
+                number = it.getString("number"),
+                address = it.getString("address"),
+                lat = it.getDouble("lat"),
+                lang = it.getDouble("lang")
+            )
+        }
+    }
+
+    suspend fun add(contact: ContactModel?): Boolean{
+        val query = "insert into contact (number, address, lat, lang) values (?, ?, ${contact?.lat}, ${contact?.lang})"
+
+        DBManager.getConnection().sendPreparedStatementAwait("delete from contact where true", arrayListOf())
+
+        DBManager.getConnection().sendPreparedStatementAwait(query, arrayListOf(
+            contact?.number,
+            contact?.address
+        ))
+
+        return true
+    }
+}
